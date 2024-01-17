8c40ce3c067a2c9b4896d05a6e78da2202e35959
mr-atuzie <user email>
2024-01-17 15:38:26 +0100

    pakan update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Service from "./pages/Service";
-import Pricing from "./pages/Pricing";
-import Contact from "./pages/Contact";
-import Blog from "./pages/Blog";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/services" element={<Service />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact-us" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Classes from "../components/Classes";
-import WhyUs from "../components/WhyUs";
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Classes />
-      <WhyUs />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
